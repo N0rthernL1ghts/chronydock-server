b63771389988449f9a88af8ff993da9f523f6f22
xZero707 <aleksandar@puharic.com>
2024-06-22 18:11:31 +0200

    Update dev image

--- build-dev.sh
+++ build-dev.sh
@@ -3,5 +3,5 @@ set -e
 
 # Build development friendly image with addition of nano and bash.
 
-docker build -t local/nlss-chronyd .
 docker build -t local/nlss-chronyd:dev . -f Dockerfile.dev
+echo "> Image tagged as local/nlss-chronyd:dev"
